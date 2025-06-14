ddf176802bdae3891e45c56b2a368d6b20bfcc30
srivenky33 <56192306+srivenky33@users.noreply.github.com>
2025-06-14 01:32:38 +0000

    fetch new one

--- demo/src/main/java/com/example/demo/HelloService.java
+++ demo/src/main/java/com/example/demo/HelloService.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Service;
 public class HelloService {
     public String greet(String name)
     {
-        return "Hello" + name + "!"+"How are you";
+        return "Hello" + name + "!"+"How are you!";
     }
 }
