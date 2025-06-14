74787037d3c7f67ec7a228f658fff2288b36cefa
srivenky33 <56192306+srivenky33@users.noreply.github.com>
2025-06-14 04:48:34 +0000

    h11

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -18,7 +18,7 @@ public class HelloController {
     @GetMapping("/hello")
     public String sayHello(@RequestParam(name="user", defaultValue="world") String user)
     {
-        //hi
+        //hii
        return helloservice.greet(user);
     }
 
